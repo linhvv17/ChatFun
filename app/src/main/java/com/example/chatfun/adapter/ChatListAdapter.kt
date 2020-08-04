626c157646a76621673b79f74d9164e45aed6fc0
HVMM17 <hvmm@HVMM17s-iMac.local>
2020-08-04 09:35:39 +0700

    update chat fragment 2

---
+++ app/src/main/java/com/example/chatfun/adapter/ChatListAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.chatfun.adapter
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatfun.MessageChatActivity
+import com.example.chatfun.R
+import com.example.chatfun.VisitUserProfileActivity
+import com.example.chatfun.model.ChatList
+import com.example.chatfun.model.User
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ChatListAdapter(
+    mContext: Context?,
+    mUsers: java.util.ArrayList<ChatList>,
+    isChatCheck: Boolean
+): RecyclerView.Adapter<ChatListAdapter.ViewHolder>() {
+    private val mContext: Context? = mContext
+    private val mUsers: ArrayList<User> = mUsers
+    private val isChatCheck: Boolean = isChatCheck
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tv_username_profile_search)
+        //        var lastMessage: TextView = itemView.findViewById(R.id.tv_last_message)
+        var imgProfile: CircleImageView = itemView.findViewById(R.id.img_view_profile_search)
+//        var imgOnline: CircleImageView = itemView.findViewById(R.id.img_view_online)
+//        var img_offline: CircleImageView = itemView.findViewById(R.id.img_view_offline)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // infalater
+        val view = LayoutInflater.from(mContext)
+            .inflate(R.layout.user_search_item_layout,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mUsers.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user: User = mUsers[position]
+        holder.tvName.text = user.getSearch()
+        Picasso.get().load(user.getProfile()).into(holder.imgProfile)
+        holder.itemView.setOnClickListener {
+            val options = arrayOf<CharSequence>(
+                "Send Message",
+                "Visit Profile"
+            )
+            val builder = AlertDialog.Builder(mContext)
+            builder.setTitle("What do you want?")
+            builder.setItems(options, DialogInterface.OnClickListener{ dialog, position ->
+                if (position == 0){
+                    val intent = Intent(mContext!!, MessageChatActivity::class.java)
+                    intent.putExtra("visit_id",user.getUid())
+                    mContext.startActivity(intent)
+                }
+                if (position == 1){
+                    val intent = Intent(mContext!!, VisitUserProfileActivity::class.java)
+                    intent.putExtra("visit_id",user.getUid())
+                    mContext.startActivity(intent)
+                }
+            })
+
+            builder.show()
+        }
+    }
+}
--- app/src/main/java/com/example/chatfun/fragment/ChatFragment.kt
+++ app/src/main/java/com/example/chatfun/fragment/ChatFragment.kt
@@ -1,8 +1,6 @@
 package com.example.chatfun.fragment
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatfun.R
-import com.example.chatfun.adapter.UserAdapter
+import com.example.chatfun.adapter.ChatListAdapter
 import com.example.chatfun.model.Chat
+import com.example.chatfun.model.ChatList
 import com.example.chatfun.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -22,8 +21,8 @@ import com.google.firebase.database.ValueEventListener
 import java.util.ArrayList
 
 class ChatFragment: Fragment() {
-    private lateinit var mUserAdapter: UserAdapter
-    private lateinit var mListChat: List<Chat>
+    private lateinit var mChatListAdapter: ChatListAdapter
+    private lateinit var mListChat: List<ChatList>
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchEdtText: EditText
     override fun onCreateView(
@@ -31,7 +30,7 @@ class ChatFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mListChat = ArrayList<Chat>()
+        mListChat = ArrayList<ChatList>()
         retrieveAllUsers()
         val view =  inflater.inflate(R.layout.chat_fragment,container,false)
         recyclerView = view.findViewById(R.id.rc_search)
@@ -40,23 +39,23 @@ class ChatFragment: Fragment() {
         recyclerView!!.layoutManager = layoutManager
         recyclerView!!.setHasFixedSize(true)
         searchEdtText = view.findViewById(R.id.edt_search_user)
-        mUserAdapter = UserAdapter(context, mListChat!! as ArrayList<User>,false)
-        recyclerView!!.adapter = mUserAdapter
+        mChatListAdapter = ChatListAdapter(context, mListChat!! as ArrayList<ChatList>,false)
+        recyclerView!!.adapter = mChatListAdapter
 
-        searchEdtText!!.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                searchForUser(s.toString().toLowerCase())
-            }
-
-        })
+//        searchEdtText!!.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                searchForUser(s.toString().toLowerCase())
+//            }
+//
+//        })
         return view
     }
 
@@ -76,38 +75,38 @@ class ChatFragment: Fragment() {
                             (mListChat as ArrayList<Chat>).add(chat)
                         }
                     }
-                    mUserAdapter = UserAdapter(context!!, mListChat!! as ArrayList<User>,false)
-                    mUserAdapter!!.notifyDataSetChanged()
-                    recyclerView!!.adapter = mUserAdapter
+                    mChatListAdapter = ChatListAdapter(context!!, mListChat!! as ArrayList<ChatList>,false)
+                    mChatListAdapter!!.notifyDataSetChanged()
+                    recyclerView!!.adapter = mChatListAdapter
                 }
             }
 
         })
 
     }
-    private fun searchForUser(str:String){
-        var firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
-        val queryUsers = FirebaseDatabase.getInstance().reference
-            .child("Users").orderByChild("search")
-            .startAt(str)
-            .endAt(str+"\uf8ff")
-        queryUsers.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                (mListChat as ArrayList<User>).clear()
-                for (dataSnapshot in p0.children) {
-                    val user: User? = dataSnapshot.getValue(User::class.java)
-                    if (user!!.getUid() != firebaseUserID) {
-                        (mListChat as ArrayList<User>).add(user)
-                    }
-                }
-                mUserAdapter = UserAdapter(context!!, mListChat!! as ArrayList<User>,false)
-                mUserAdapter!!.notifyDataSetChanged()
-                recyclerView!!.adapter = mUserAdapter
-            }
-        })
-    }
+//    private fun searchForUser(str:String){
+//        var firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
+//        val queryUsers = FirebaseDatabase.getInstance().reference
+//            .child("Users").orderByChild("search")
+//            .startAt(str)
+//            .endAt(str+"\uf8ff")
+//        queryUsers.addValueEventListener(object : ValueEventListener {
+//            override fun onCancelled(p0: DatabaseError) {
+//
+//            }
+//
+//            override fun onDataChange(p0: DataSnapshot) {
+//                (mListChat as ArrayList<User>).clear()
+//                for (dataSnapshot in p0.children) {
+//                    val user: User? = dataSnapshot.getValue(User::class.java)
+//                    if (user!!.getUid() != firebaseUserID) {
+//                        (mListChat as ArrayList<User>).add(user)
+//                    }
+//                }
+//                mUserAdapter = UserAdapter(context!!, mListChat!! as ArrayList<User>,false)
+//                mUserAdapter!!.notifyDataSetChanged()
+//                recyclerView!!.adapter = mUserAdapter
+//            }
+//        })
+//    }
 }
